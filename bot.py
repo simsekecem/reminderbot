3dd9bbc643d00579d131cc1ad603ec88afbab721
simsekecem <67986398+simsekecem@users.noreply.github.com>
2023-08-20 21:14:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,11 @@ async def on_ready():
 
     # Bu kısım botun belirli bir zaman aralığında çalışmasını sağlar
     while True:
-        channel = client.get_channel(1123444669708238900)  # Kanal ID'sini buraya girin
+        channel = client.get_channel(xxxx)  # Kanal ID'sini buraya girin
         await channel.send("@everyone 🆘 Hourly reminder 🆘 Increase the gap! https://shiningawards.com/best-kpop-rapper-2023/")
 
         # 1 saat beklemek için
         await asyncio.sleep(3600)
 
 
-client.run('MTE0Mjg1NTQxOTg1Nzk5Mzc5MA.GtHmOr.h-qsSLMW9X3jV_Cqi2ZLilhJuRGNNvizzJmtvg')
+client.run('xxx')
